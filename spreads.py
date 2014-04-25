5f43918f2272c27e09311228ab87c58dcbe74759
William Schwartz <wkschwartz@gmail.com>
2014-04-25 01:15:47 -0400

    Add code and tests for reading one game at a time

---
+++ spreads.py
@@ -0,0 +1,56 @@
+import pandas as pd
+from pandas.io.html import read_html
+
+
+def one_game_url(team_a, team_b, week, year):
+	"Calculate the URL for the spreads from team_a to team_b."
+	base = "http://www.teamrankings.com/nfl/matchup/"
+	template = "{team_a}-{team_b}-week-{week}-{year}"
+	tail = "/spread-movement"
+	result = template.format(team_a=team_a, team_b=team_b, week=week, year=year)
+	return ''.join([base, result, tail])
+
+
+def one_game_table(team_a, team_b, week, year):
+	"Download, parse, and clean the spreads table for one game."
+	# Note that infer_types is deprecated and won't work starting in Pandas 0.14
+	data = read_html(io=one_game_url(team_a, team_b, week, year),
+					 match="History", attrs={'id': 'table-000'},
+					 infer_types=False, header=0,
+					 skiprows=[1, 2, 3])
+	if len(data) != 1:
+		raise ValueError("Couldn't find the correct table.")
+	data = data.pop()
+
+	# Cleaning.
+	# Parse the date and set it as the index for the dataframe
+	data['datetime'] = pd.to_datetime(data['Unnamed: 0'].replace(
+		r'(\d\d?/\d\d?)', r'\1/' + str(year), regex=True))
+	del data['Unnamed: 0']
+	data.set_index(keys='datetime', inplace=True, verify_integrity=True)
+
+	# Replace all the '--' as missing so we can convert numbers to floats.
+	for column in data.keys():
+		data[column] = data[column].replace('--', '').apply(
+			lambda s: float(s) if s else float('nan'))
+
+	# Add this function's arguments to the table.
+	data['team_a'] = team_a
+	data['team_b'] = team_b
+	data['week'] = week
+
+	# Lowercase column names for ease of programming later
+	data.columns = [h.lower() for h in data.columns]
+
+	return data
+
+
+def concatenate_tables(tables):
+	"Append an iterable of tables together."
+	last = None
+	for table in tables:
+		if last is None:
+			last = table
+		else:
+			last = last.append(table)
+	return last
---
+++ test_spreads.py
@@ -0,0 +1,48 @@
+import unittest
+
+import numpy as np
+import pandas as pd
+
+import spreads
+
+
+class TestOneGame(unittest.TestCase):
+
+	def test_one_game_url(self):
+		self.assertEqual(
+			spreads.one_game_url('ravens', 'broncos', 1, 2013),
+			"http://www.teamrankings.com/nfl/matchup/ravens-broncos-week-1-2013/spread-movement")
+
+	def assert_columns(self, data, year):
+		# Date: check type, check year, check uniquness of index
+		self.assertIsInstance(data.index, pd.tseries.index.DatetimeIndex)
+		for date in data.index:
+			self.assertEqual(date.year, year)
+		self.assertEqual(len(data.index), len(set(data.index)))
+		# pinnacle, betonline, bookmaker: it's a float
+		for column in 'pinnacle', 'betonline', 'bookmaker':
+			self.assertIs(data[column].dtype, np.dtype('float64'))
+		# Has three other columns
+		for col in 'team_a', 'team_b', 'week':
+			self.assertIn(col, data.keys())
+
+	def test_one_game_table(self):
+		team_a, team_b, week, year = 'ravens', 'broncos', 1, 2013
+		data = spreads.one_game_table(team_a, team_b, week, year)
+		self.assert_columns(data, year)
+		# team_a=ravens, team_b=broncos, week=1
+		for x in data.team_a:
+			self.assertEqual(x, team_a)
+		for x in data.team_b:
+			self.assertEqual(x, team_b)
+		for x in data.week:
+			self.assertEqual(x, week)
+
+	def test_concatenate_tables(self):
+		# Get two tables
+		year = 2013
+		one = spreads.one_game_table('ravens', 'broncos', 1, year)
+		two = spreads.one_game_table('ravens', 'bengals', 17, year)
+		data = spreads.concatenate_tables([one, two])
+		self.assert_columns(data, year)
+		self.assertEqual(len(one) + len(two), len(data))
